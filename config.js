d98a77427894307342cb89802d3f777d52c630fe
AgentXbox770 <fonteneaumonika7@gmail.com>
2026-04-16 02:33:07 +0200

    Add Firebase configuration to config.js

---
+++ config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCXhWdKsuOWqI9mejUx3khzrvibVWJKBz8",
+    authDomain: "aerochat-c16d6.firebaseapp.com",
+    databaseURL: "https://aerochat-c16d6-default-rtdb.firebaseio.com",
+    projectId: "aerochat-c16d6",
+    storageBucket: "aerochat-c16d6.firebasestorage.app",
+    messagingSenderId: "417272842430",
+    appId: "1:417272842430:web:e6985a117a7c27d228f6ef"
+};
