621f1ac8c1713a57a9fc2a52691f99fa658304a7
RoVerify <maria.gomes23.1949@gmail.com>
2026-04-21 17:14:56 -0300

    Update Firebase configuration details

--- config.js
+++ config.js
@@ -1,8 +1,10 @@
+
 const firebaseConfig = {
-  apiKey: "AIzaSyDleY28-z5sSpCWoPHIPi43fqwqHTNONl0",
-  authDomain: "newaerochat.firebaseapp.com",
-  projectId: "newaerochat",
-  storageBucket: "newaerochat.firebasestorage.app",
-  messagingSenderId: "838837161152",
-  appId: "1:838837161152:web:f7800243e005892329b125"
+  apiKey: "AIzaSyC14jAkQW_TgaRwX6YYWf5lP04L8jfL7Wc",
+  authDomain: "aerochat-98a65.firebaseapp.com",
+  projectId: "aerochat-98a65",
+  storageBucket: "aerochat-98a65.firebasestorage.app",
+  messagingSenderId: "384401771067",
+  appId: "1:384401771067:web:9b77b01ba2ef58bf8d6a1e",
+  measurementId: "G-E57L77QF58"
 };
