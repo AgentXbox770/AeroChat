fabc3eb984fe3c732f0bd3c2a988593f9ceba2b1
RoVerify <maria.gomes23.1949@gmail.com>
2026-04-21 17:24:50 -0300

    Add databaseURL to Firebase configuration

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
-
 const firebaseConfig = {
   apiKey: "AIzaSyC14jAkQW_TgaRwX6YYWf5lP04L8jfL7Wc",
   authDomain: "aerochat-98a65.firebaseapp.com",
+  databaseURL: "https://aerochat-98a65-default-rtdb.firebaseio.com",
   projectId: "aerochat-98a65",
   storageBucket: "aerochat-98a65.firebasestorage.app",
   messagingSenderId: "384401771067",
