50a3dd695ff6c413db1caf28529f8b3c8df19a7b
RoVerify <maria.gomes23.1949@gmail.com>
2026-04-22 14:00:55 -0300

    Refactor firebase config to use decoded values

--- config.js
+++ config.js
@@ -1,21 +1,19 @@
-const _0xaf42 = {
-    'a': 'QUl6YVN5QzE0akFrUVdfVGdhUndYNllZV2Y1bFAwNEw4amZMN1dj',
-    'b': 'YWVyb2NoYXQtOThhNjUuZmlyZWJhc2VhcHAuY29t',
-    'c': 'aHR0cHM6Ly9hZXJvY2hhdC05OGE2NS1kZWZhdWx0LXJ0ZGIuZmlyZWJhc2lvLmNvbQ==',
-    'd': 'YWVyb2NoYXQtOThhNjU=',
-    'e': 'YWVyb2NoYXQtOThhNjUuZmlyZWJhc2VzdG9yYWdlLmFwcA==',
-    'f': 'Mzg0NDAxNzcxMDY3',
-    'g': 'MTozODQ0MDE3NzEwNjc6d2ViOjliNzdiMDFiYTJlZjU4YmY4ZDZhMWU=',
-    'h': 'Ry1FNTdMNzdRRjU4'
-};
+const _0x4f2a = atob('QUl6YVN5QzE0akFrUVdfVGdhUndYNllZV2Y1bFAwNEw4amZMN1dj');
+const _0x8b1c = atob('YWVyb2NoYXQtOThhNjUuZmlyZWJhc2VhcHAuY29t');
+const _0x3d9e = atob('aHR0cHM6Ly9hZXJvY2hhdC05OGE2NS1kZWZhdWx0LXJ0ZGIuZmlyZWJhc2Vpby5jb20=');
+const _0x7a2f = atob('YWVyb2NoYXQtOThhNjU=');
+const _0x1e8d = atob('YWVyb2NoYXQtOThhNjUuZmlyZWJhc2VzdG9yYWdlLmFwcA==');
+const _0x9c3b = atob('Mzg0NDAxNzcxMDY3');
+const _0x5f6a = atob('MTozODQ0MDE3NzEwNjc6d2ViOjliNzdiMDFiYTJlZjU4YmY4ZDZhMWU=');
+const _0x2d4c = atob('Ry1FNTNMN0ZRNTQ=');
 
 const firebaseConfig = {
-    apiKey: atob(_0xaf42.a),
-    authDomain: atob(_0xaf42.b),
-    databaseURL: atob(_0xaf42.c),
-    projectId: atob(_0xaf42.d),
-    storageBucket: atob(_0xaf42.e),
-    messagingSenderId: atob(_0xaf42.f),
-    appId: atob(_0xaf42.g),
-    measurementId: atob(_0xaf42.h)
+  apiKey: _0x4f2a,
+  authDomain: _0x8b1c,
+  databaseURL: _0x3d9e,
+  projectId: _0x7a2f,
+  storageBucket: _0x1e8d,
+  messagingSenderId: _0x9c3b,
+  appId: _0x5f6a,
+  measurementId: _0x2d4c
 };
