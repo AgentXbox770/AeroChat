165ce797b56e3857fa29a06e57ce905ef8f1085d
RoVerify <maria.gomes23.1949@gmail.com>
2026-04-22 13:56:46 -0300

    Obfuscate Firebase configuration in config.js
    Replaced hardcoded Firebase configuration with obfuscated values decoded at runtime.

--- config.js
+++ config.js
@@ -1,10 +1,21 @@
+const _0xaf42 = {
+    'a': 'QUl6YVN5QzE0akFrUVdfVGdhUndYNllZV2Y1bFAwNEw4amZMN1dj',
+    'b': 'YWVyb2NoYXQtOThhNjUuZmlyZWJhc2VhcHAuY29t',
+    'c': 'aHR0cHM6Ly9hZXJvY2hhdC05OGE2NS1kZWZhdWx0LXJ0ZGIuZmlyZWJhc2lvLmNvbQ==',
+    'd': 'YWVyb2NoYXQtOThhNjU=',
+    'e': 'YWVyb2NoYXQtOThhNjUuZmlyZWJhc2VzdG9yYWdlLmFwcA==',
+    'f': 'Mzg0NDAxNzcxMDY3',
+    'g': 'MTozODQ0MDE3NzEwNjc6d2ViOjliNzdiMDFiYTJlZjU4YmY4ZDZhMWU=',
+    'h': 'Ry1FNTdMNzdRRjU4'
+};
+
 const firebaseConfig = {
-  apiKey: "AIzaSyC14jAkQW_TgaRwX6YYWf5lP04L8jfL7Wc",
-  authDomain: "aerochat-98a65.firebaseapp.com",
-  databaseURL: "https://aerochat-98a65-default-rtdb.firebaseio.com",
-  projectId: "aerochat-98a65",
-  storageBucket: "aerochat-98a65.firebasestorage.app",
-  messagingSenderId: "384401771067",
-  appId: "1:384401771067:web:9b77b01ba2ef58bf8d6a1e",
-  measurementId: "G-E57L77QF58"
+    apiKey: atob(_0xaf42.a),
+    authDomain: atob(_0xaf42.b),
+    databaseURL: atob(_0xaf42.c),
+    projectId: atob(_0xaf42.d),
+    storageBucket: atob(_0xaf42.e),
+    messagingSenderId: atob(_0xaf42.f),
+    appId: atob(_0xaf42.g),
+    measurementId: atob(_0xaf42.h)
 };
